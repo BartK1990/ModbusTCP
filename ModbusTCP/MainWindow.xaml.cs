4ccdf7452afbdedbf4b126850467cfec1dc42caf
Bartosz Kuriata <raximek@gmail.com>
2019-01-14 22:27:08 +0100

    Ip input solved

--- ModbusTCP/MainWindow.xaml.cs
+++ ModbusTCP/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace ModbusTCP
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        string lastIpAddressPart;
         MBTCPConnManager mbtcpConnManager;
 
         public MainWindow()
@@ -40,10 +40,21 @@ namespace ModbusTCP
             }
         }
 
-        public static bool IsValid(string str)
+        private void IPInputHandling(object sender, TextBox nextTb, TextCompositionEventArgs e)
         {
-            int i;
-            return int.TryParse(str, out i) && i >= 5 && i <= 9999;
+            if (e.Text == "." && nextTb != null)
+                nextTb.Focus();
+            e.Handled = !((int.TryParse(e.Text, out int i)) && IPPartInRange(i)); // checks if input is number 0...255
+            if (sender is TextBox)
+            {
+                TextBox tb = sender as TextBox;
+                lastIpAddressPart = tb.Text;
+            }
+        }
+
+        private bool IPPartInRange(int i)
+        {
+            return (i >= 0 && i <= 255) ? true : false;
         }
 
         private void SetIP_Click(object sender, RoutedEventArgs e)
@@ -72,7 +83,38 @@ namespace ModbusTCP
 
         private void IPAddr1_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            e.Handled = !IsValid(((TextBox)sender).Text + e.Text);
+            IPInputHandling(sender, IPAddr2, e);
+        }
+
+        private void IPAddr2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            IPInputHandling(sender, IPAddr3, e);
+        }
+
+        private void IPAddr3_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            IPInputHandling(sender, IPAddr4, e);
+        }
+
+        private void IPAddr4_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            IPInputHandling(sender, null, e);
+        }
+
+        private void IPPort_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !((int.TryParse(e.Text, out int i)) && i >= 0 && i <= 65536); // checks if input is number 0...65536
+        }
+
+        private void IPAddr1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender is TextBox)
+            {
+                TextBox tb = sender as TextBox;
+                if (int.TryParse((tb.Text), out int i))
+                    if(!IPPartInRange(i))
+                        tb.Text = lastIpAddressPart;
+            }      
         }
     }
 }
