1f3c5438e4f7f4098154d5421cc0050a1c319994
Bartosz Kuriata <raximek@gmail.com>
2019-03-24 22:10:25 +0100

    MBTCPMessages static class added

---
+++ ModbusTCP/Model/MBTCPMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusTCP.Model
+{
+
+
+    public static class MBTCPMessages
+    {
+
+
+
+
+        public static Byte[] ReadHoldingRegisterSend(int startAddress, int quantity)
+        {
+            Byte[] message = new Byte[1];
+
+            return message;
+        }
+        public static List<int> ReadHoldingRegisterReceive(Byte[] response)
+        {
+            List<int> registersValues = new List<int>();
+
+            return registersValues;
+        }
+    }
+}
