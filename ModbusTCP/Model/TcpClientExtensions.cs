9dd86b73e667551c52cb29ea9ded8881141fbd3a
Bartosz Kuriata <kuriata.bartosz@hotmail.com>
2020-04-05 20:01:33 +0200

    Added Extension method fot TcpClient - TcpClientExtensions

---
+++ ModbusTCP/Model/TcpClientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusTCP.Model
+{
+    public static class TcpClientExtensions
+    {
+        public static TcpState GetState(this TcpClient tcpClient)
+        {
+            if (tcpClient.Client == null)
+                return TcpState.Closed;
+            try
+            {
+                var connectionInformation = IPGlobalProperties.GetIPGlobalProperties()
+                    .GetActiveTcpConnections()
+                    .FirstOrDefault(x => x.LocalEndPoint.Equals(tcpClient.Client.LocalEndPoint));
+                return connectionInformation?.State ?? TcpState.Unknown;
+            }
+            catch
+            {
+                return TcpState.Closed;
+            }
+        }
+    }
+}
